09060950ab3c5ccd238bd6c82e5e1a43eead81f9
Jan Orel <jorel@opennebula.systems>
2021-07-22 11:32:38 +0200

    multipart

--- mail.py
+++ mail.py
@@ -1,29 +1,41 @@
 #!/usr/bin/env python3
 
 import smtplib, ssl, sys
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 
 port = 587  # For starttls
-smtp_server = 'mail.opennebula.org'
+smtp_server = 'smtp.dreamhost.com'
 sender_email = 'infra@opennebula.org'
 receiver_email = 'jorel@opennebula.io'
 password = sys.argv[1]
 
-message = """\
-Subject: Test email1
+message = MIMEMultipart('text')
+message["Subject"] = "python multipart"
+message["From"] = sender_email
+message["To"] = receiver_email
 
-This message is sent from Python."""
+text = """\
+here
+is
+something
+"""
 
-#context = ssl.create_default_context()
-context = ssl._create_unverified_context()
+part1 = MIMEText(text, "plain")
+
+message.attach(part1)
+
+context = ssl.create_default_context()
 
 with smtplib.SMTP(smtp_server, port) as server:
-    server.ehlo()  # Can be omitted
+    server.ehlo('127.0.0.1')  # Can be omitted
 
     server.starttls(context=context)
 
     server.ehlo()  # Can be omitted
     server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+    server.sendmail(sender_email, receiver_email, message.as_string())
+    server.close()
 
 print("OK")
