86c571f2bab9e441dd5fb72d05cf823557bc7415
Jan Orel <jorel@opennebula.systems>
2021-09-13 10:12:48 +0200

    test-faraday

--- test.rb
+++ test.rb
@@ -1,19 +1,7 @@
 #!/usr/bin/ruby
-#
-def set_output(name, content)
-    # escape content
-    esc = content.gsub("%", '%25')
-    esc.gsub!("\n", '%0A')
-    esc.gsub!("\r", '%0D')
 
-    puts "::set-output name=#{name}::#{esc}"
-end
+require "faraday"
+require "json"
 
-puts "AAA"
-puts "BBB"
-
-
-set_output("test1", "abc")
-set_output("test2", "aaa\nbbb")
-set_output("test3", "aaa\nbbb\nccc")
-
+resp =  Faraday.get('http://demo.opennebula.com/')
+pp resp.status
